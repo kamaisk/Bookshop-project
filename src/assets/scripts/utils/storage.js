39919dbc6153800464b596f62ace78cc79c3b16a
Kamilla <kamilla.iskandarova.98@mail.ru>
2025-03-31 12:50:46 +1000

    fixed js-files

--- src/assets/scripts/utils/storage.js
+++
@@ -1,9 +0,0 @@
-// // Сохранить товары в localStorage
-// export function saveToStorage(key, value) {
-//   localStorage.setItem(key, JSON.stringify(value));
-// }
-
-// // Загрузить товары из localStorage
-// export function loadFromStorage(key) {
-//   return JSON.parse(localStorage.getItem(key));
-// }
