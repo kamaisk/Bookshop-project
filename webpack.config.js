90e7375a90fcaec0b7a442d9d7361c9d87d54c39
Kamilla <kamilla.iskandarova.98@mail.ru>
2025-04-08 18:10:13 +1000

    fixed publicPath in webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   // entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    // publicPath: "/",
+    publicPath: "./",
     // filename: "bundle.js",
   },
 
